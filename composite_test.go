2526adce26dbfd4b2991c1d92e89a97ae3333ba4
Andre Soares <andregsilv@gmail.com>
2022-10-27 10:52:56 -0300

    composite job result

--- composite_test.go
+++ composite_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCompositeJob_StartAndWait(t *testing.T) {
@@ -54,4 +55,40 @@ func TestCompositeJob_StartAndWait(t *testing.T) {
 		Return(fakeError2)
 	compositeJob.Stop(ctxStop)
 	<-stop
+}
+
+func TestCompositeJob_WhenMultipleStartErrors_ShouldGroupAll(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	fakeError1 := errors.New(gofakeit.SentenceSimple())
+	fakeError2 := errors.New(gofakeit.SentenceSimple())
+
+	jobMock1 := NewMockJob(ctrl)
+	jobMock1.EXPECT().
+		Start(context.TODO()).
+		Return(fakeError1)
+	jobMock1.EXPECT().
+		Stop(context.TODO()).
+		Return(nil)
+
+	jobMock2 := NewMockJob(ctrl)
+	jobMock2.EXPECT().
+		Start(context.TODO()).
+		Return(fakeError2)
+	jobMock2.EXPECT().
+		Stop(context.TODO()).
+		Return(nil)
+
+	compositeJob := CompositeJob{
+		Jobs: []Job{
+			jobMock1,
+			jobMock2,
+		},
+	}
+
+	err := compositeJob.Start(context.TODO())
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, fakeError1)
+	assert.ErrorIs(t, err, fakeError2)
 }
